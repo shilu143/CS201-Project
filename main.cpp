c87dd93915dfe17750ab45c03befb9897a31fad3
Shilu Tudu <shilutudu123@gmail.com>
2021-10-12 05:16:15 +0530

    cwh commit

--- main.cpp
+++ main.cpp
@@ -2,16 +2,14 @@
 using namespace std;
 int main() {
     int t;
-    cin >> t;
     while (t--) {
         int n;
         cin >> n;
         int a[n];
         for (int i = 0;i < n;i++)
             cin >> a[i];
-        sort(a, a + n);
-        for (int i = 0;i < n;i++)
-            cout << a[i] << " ";
+        for (auto x : a)
+            cout << x << " ";
         cout << "\n";
     }
     return 0;
